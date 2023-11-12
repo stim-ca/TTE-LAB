98ce81d95180bc7b0626c4bea4ed743e3f71697f
Shawn Timewell <Shawn.Timewell@timewelltech.ca>
2023-11-12 15:37:39 -0800

    added New-Splat command by cody

---
+++ powershell/New-Splat.ps1
@@ -0,0 +1,30 @@
+# Made by Co-work Cody
+Function New-Splat {
+    [CmdletBinding()]
+    Param
+    (
+        [string][Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $True,
+            Position = 0)]
+        [ValidateNotNullOrEmpty()]
+        $Name
+    )
+
+    try {
+
+        $Parameters = (get-command -name $Name).parameters | Out-GridView -PassThru | Select-Object Name
+        
+        $output = "`$$($Name.Replace('-', ''))_Properties = {`n"
+
+        foreach ($parameter in $parameters) {
+            $output += "`t$($parameter.Name) = `"`"`n"
+        }
+
+        $output += "}"
+        Write-Host $output
+
+    }
+    catch {
+
+    }
+}
